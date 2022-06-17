63c5311acb3809cac64ea10ca3240fd329f35213
Bilal Khan <38106396+devbilalkhan@users.noreply.github.com>
2022-06-17 11:13:16 +0000

    Add test post api

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
+from fastapi.params import Body 
 
 app = FastAPI()
 
+
 @app.get("/")
 async def root():
+    return {"message": "My first fastAPI"}
+
+@app.post("/name")
+async def name(payload: dict = Body(...)):
     return {
-        "message": "My first fastAPI"
+        "message" : payload
     }
